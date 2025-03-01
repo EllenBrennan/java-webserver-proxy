7719ea0c6dea7a384ec9dcece503025fc6e28866
brenne15 <brennanellen228@gmail.com>
2025-03-01 13:46:50 +0000

    Http handling

--- Proxy.java
+++ Proxy.java
@@ -1,22 +1,30 @@
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 /*
 Opens a socket listening on portNumber 
  */
 
 public class Proxy {
-    static int portNumber = 4000; 
+    int portNumber = 4000; 
+    ArrayList<String> blockedSites;
 
     public static void main(String[] args) {
         try {
-            //listening out on for client requests at port number 
-            ServerSocket server = new ServerSocket(portNumber);
+            
+            Proxy myProxy = new Proxy();
+            
+            //listening out on for client requests at port number
+            ServerSocket server = new ServerSocket(myProxy.portNumber);
+            myProxy.blockedSites = new ArrayList<String>();
             boolean isOn = true; // will be controlled by console later
+        
 
             while(isOn){
                 Socket client = server.accept();
-                new Thread(new Requests(client)).start(); //open request on individual thread
+                Thread currentRequest = new Thread(new Requests(client, myProxy.blockedSites)); //open request on individual thread 
+                currentRequest.start();
                 
             }
             server.close(); //once isOn false close server
--- Requests.java
+++ Requests.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 /**
- * Handles individual client requests n their own thread
+ * Handles individual client requests in its own thread
  */
 
 public class Requests implements  Runnable{
@@ -12,9 +15,12 @@ public class Requests implements  Runnable{
     Socket client;
     InputStreamReader cIn;
     BufferedReader cInReader;
+    OutputStreamWriter      cOut;
+    ArrayList <String> bannedSites;
 
-    Requests(Socket client){
+    Requests(Socket client, ArrayList<String>bannedSites){
         this.client = client;
+        this.bannedSites = bannedSites;
 
     }
 
@@ -23,17 +29,88 @@ public class Requests implements  Runnable{
       try {
         cIn = new InputStreamReader(client.getInputStream());
         cInReader = new BufferedReader(cIn);
-        System.out.println(cInReader.readLine());
+        cOut = new OutputStreamWriter(client.getOutputStream());
 
+
+        //processing request by first extracing url then obtaining request as string
+        String line = cInReader.readLine(); //retrieve the request line
+        ArrayList <String> req = new ArrayList<>();
+        String current = line;
+
+        //putting each line of the request into an arraylist
+        while(current != null){
+            req.add(current);
+            current = cInReader.readLine();
+
+        }
+
+        String url = getUrl(line);
+        System.out.println("current host is " + url);
+        System.out.println(req);
+        if(!bannedSites.contains(url)){ 
+          //if site from request not banned, make socket  to connect to webserver
+          Socket server = new Socket(url, 80);
+
+          //write request to server
+          BufferedWriter writeToServer = new BufferedWriter(
+            new OutputStreamWriter(server.getOutputStream()));
+            
+
+            //for each line of the request, write 
+            System.out.println("writing request out: ");
+            for(String x : req){
+              writeToServer.write(x);
+              System.out.println(x);
+            }
+            writeToServer.flush();
+
+          //read server's response
+          BufferedReader readFromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+          String reply;
+          while ((reply = readFromServer.readLine())!= null){
+            cOut.write(reply + "\r\n");
+            cOut.flush();
+          }
+
+
+
+          server.close();
+          client.close();
+
+        }
+        else{//site at url banned
+            cOut.write("Url banned");
+            cOut.flush();
+
+
+        }
+        
 
 
       } catch (Exception e) {
+        e.printStackTrace();
 
       }
       
 
       
     }
+
+    //helper methods
+
+    //Get Url
+    //takes a request line as a parameter
+    //ie CONNECT www.somewebsite.com:100 HTTP/1.1
+    //splits it by spaces to take www.somewebsite.com:100 out
+    //extracts string from start to before : to get just the link and not the port number
+    private String getUrl(String reqLine){
+        String url_and_port = reqLine.split(" ")[1];
+        int portStartIndex = url_and_port.indexOf(':');
+        String url = url_and_port.substring(0, portStartIndex);
+        return url;
+        
+    }
+
 
     
     
